7d03145b21fb931186a08844991db3cbba00537e
yuchanns <airamusume@gmail.com>
2020-02-01 20:53:47 +0800

    Add assembly/arr_slice

---
+++ assembly/arr_slice.go
@@ -0,0 +1,7 @@
+package assembly
+
+func ArraySlice() {
+	arr := []int{1, 2, 4}
+	sl := arr[:]
+	_ = sl
+}
--- assembly/range_clause.go
+++ assembly/range_clause.go
@@ -3,7 +3,7 @@ package assembly
 import "fmt"
 
 func RangeClause() {
-	arr := []int{1, 2, 3}
+	arr := []int{1, 2, 4}
 	var newArr []*int
 	for _, v := range arr {
 		newArr = append(newArr, &v)
