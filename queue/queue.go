24accf8ed077be69080b263b801ef64da40e1715
yuchanns <airamusume@gmail.com>
2020-03-14 23:34:43 +0800

    remove todo

--- queue/queue.go
+++ queue/queue.go
@@ -16,7 +16,6 @@ type IMessage interface {
 }
 
 // Queue can produce and consume messages
-// TODO: A Queue should have context to cancel the consumer process
 type Queue struct {
 	pool *redis.Pool
 }
