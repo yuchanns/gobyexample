6b3357e09b76e7635762ec59c6b90b834b2f1a63
yuchanns <airamusume@gmail.com>
2020-09-22 22:43:38 +0800

    add ants

--- cbc/pkg_test.go
+++ cbc/pkg_test.go
@@ -2,6 +2,8 @@ package cbc
 
 import (
 	"github.com/coreos/etcd/pkg/testutil"
+	"github.com/panjf2000/ants/v2"
+	"sync"
 	"testing"
 )
 
@@ -17,4 +19,64 @@ func TestEncryptDecrypt(t *testing.T) {
 
 	testutil.AssertNil(t, err)
 	testutil.AssertEqual(t, toEncryptBytes, decryptedBytes)
+}
+
+func BenchmarkDecrypt(b *testing.B) {
+	b.StopTimer()
+
+	keyCoder, _ := NewKeyCoder()
+
+	toEncryptBytes := []byte("This is a string to encrypt")
+	encryptedBytes, iv, _ := keyCoder.Encrypt(toEncryptBytes)
+
+	wg := &sync.WaitGroup{}
+	b.StartTimer()
+
+	for t := 0; t <= 59; t++ {
+		for i := 0; i < 13; i++ {
+			wg.Add(1)
+			go func() {
+				decryptedBytes, _ := keyCoder.Decrypt(encryptedBytes, keyCoder.Key(), iv)
+				_ = decryptedBytes
+				wg.Done()
+			}()
+		}
+		//time.Sleep(time.Second)
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkPoolDecrypt(b *testing.B) {
+	defer ants.Release()
+	b.StopTimer()
+
+	keyCoder, _ := NewKeyCoder()
+
+	toEncryptBytes := []byte("This is a string to encrypt")
+	encryptedBytes, iv, _ := keyCoder.Encrypt(toEncryptBytes)
+
+	wg := sync.WaitGroup{}
+
+	decryptFunc := func() {
+		decryptedBytes, _ := keyCoder.Decrypt(encryptedBytes, keyCoder.Key(), iv)
+		_ = decryptedBytes
+	}
+
+	p, _ := ants.NewPoolWithFunc(5, func(i interface{}) {
+		decryptFunc()
+		wg.Done()
+	})
+	defer p.Release()
+
+	for t := 0; t <= 59; t++ {
+		b.StartTimer()
+		for i := 0; i < 13; i++ {
+			wg.Add(1)
+			_ = p.Invoke(i)
+		}
+		b.StopTimer()
+		//time.Sleep(time.Second)
+	}
+	wg.Wait()
 }
--- go.mod
+++ go.mod
@@ -30,6 +30,7 @@ require (
 	github.com/mattn/go-isatty v0.0.11 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/panjf2000/ants/v2 v2.4.2
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/common v0.13.0 // indirect
